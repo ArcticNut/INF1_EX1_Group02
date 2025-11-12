af1845a7d9886f1eb2fae6bcdbe2e7f0c9e8f461
Emil Rauch <rauchemil@web.de>
2025-11-12 16:06:46 +0100

    start class room

--- Classes/Room.cs
+++ Classes/Room.cs
@@ -8,5 +8,13 @@ namespace INF1_EX1_Group02.Classes
 {
     internal class Room
     {
+        //Attributes
+        private int id;
+        private double area;
+        private Use use;
+        private List<Furniture> furnitures;
+        private double slabThickness;
+        private double slabVol;
+
     }
 }
