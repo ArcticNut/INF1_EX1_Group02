ad0b2e9ee858d10d1e1e2dc3429f3f9ca9d44df2
Emil Rauch <rauchemil@web.de>
2025-11-14 11:26:31 +0100

    Finish Furniture and Use

--- Classes/Use.cs
+++ Classes/Use.cs
@@ -8,5 +8,28 @@ namespace INF1_EX1_Group02.Classes
 {
     internal class Use
     {
+        // Attributes
+        private static int id = 0;
+        private string name;
+        private double qk; //live load in kN/m²
+
+        public Use(string name, double qk)
+        {
+            id = id++;
+            this.name = name;
+            this.qk = qk;
+        }
+
+        // Getters and Setters
+        public int Id { get { return id; } }
+        public string Name { get { return name; } set { name = value; } }
+        public double Qk { get { return qk; } set { qk = value; } }
+
+        // Methods
+        public override string ToString()
+        {
+            return name;
+        }
+
     }
 }
